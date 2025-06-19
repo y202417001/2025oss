6c9c3d4cf400d88ae0635fc4a0925d334400fbdc
202207003 <l342887k@yuhan.ac.kr>
2025-06-19 11:43:41 +0900

    [ * ]

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -13,13 +13,24 @@ int main()
 {
     int x = 0;
     int y = 0;
+    int i = 0;
 
-    std::cout << "첫번째 수를 입력하세요 : ";
-    std::cin >> x;
-    std::cout << "두번째 수를 입력하세요 : ";
-    std::cin >> y;
+    for (; i < 10; i++)
+    {
+        std::cout << "첫 번째 수를 입력하세요 : ";
+        std::cin >> x;
 
-    printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
-    printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n", 10, 6, 16, 4, 0);
-        //add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
+        if (-999 == x)
+        {
+            printf("프로그램을 종료합니다.\n");
+            break;
+        }
+        std::cout << "두번째 수를 입력하세요 : ";
+        std::cin >> y;
+
+        printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
+        printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n\n",
+            add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
+
+    }
 }
